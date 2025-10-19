c2f3a2408f045431068db1688ab778ffd1d2b283
discordboyy <mertygrul.28@gmail.com>
2025-10-19 11:59:08 +0200

    Delete api.js

--- src/api.js
+++
@@ -1,5 +0,0 @@
-export async function fetchRate(from, to) {
-  const response = await fetch(`http://localhost:5000/api/rate?from=${from}&to=${to}`);
-  if (!response.ok) throw new Error("Failed to fetch exchange rate");
-  return response.json();
-}
